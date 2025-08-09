1802e96709fb752e3ac7e10bc7fde287772eb41f
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-09 12:09:54 +0530

    add

---
+++ C++ Learning/Basic Program/GCD.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num,count = 0   ;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int i=1;i<=num;i++){
+        
+    }
+
+}
