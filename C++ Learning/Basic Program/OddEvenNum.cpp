99e8e08c479f3591b1fca682a70a20dab1d5206a
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-09 11:29:15 +0530

    Add

---
+++ C++ Learning/Basic Program/OddEvenNum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if(num % 2 == 0) {
+        cout << num << " is an even number." << endl;
+    } else {
+        cout << num << " is an odd number." << endl;
+    }
+}
