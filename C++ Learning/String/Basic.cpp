8d6002ce6d69089abf4e642133de904183c9b854
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-11 13:24:57 +0530

    Add

---
+++ C++ Learning/String/Basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "You entered: " << str << endl;
+    return 0;
+}/*Enter a string: Hi Im Aravinth.S
+You entered: Hi Im Aravinth.S
+*/
