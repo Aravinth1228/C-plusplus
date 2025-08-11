71d4c43be808fcfd4eedb5acc7207fe50d7fe784
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-11 13:09:04 +0530

    Add

---
+++ C++ Learning/Basic Program/Add Two Number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout <<" Enter the Two Numbers:" << endl;
+    cin >> a >> b;
+    cout << "Sum of " << a << " and " << b << " is: " << a + b << endl;
+    return 0;
+}
+/*45
+54
+Sum of 45 and 54 is: 99
+*/
